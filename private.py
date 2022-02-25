66c103f79190b8214e5bc2386d871a02e1d520f6
abbylloyd03 <abbylloyd03@gmail.com>
2022-02-25 14:28:32 -0600

    Update Chapter 10 Notes

---
+++ private.py
@@ -0,0 +1,10 @@
+# private.py
+"""Class with public and private attributes."""
+
+class PrivateClass:
+    """Class with public and private attributes."""
+    
+    def __init__(self):
+        """Initialize the public and private attributes."""
+        self.public_data = 'public'
+        self.__private_data = 'private'
