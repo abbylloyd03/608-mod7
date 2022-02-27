2b7a267e61490c0dd61815f98d715d1f46eef90d
abbylloyd03 <abbylloyd03@gmail.com>
2022-02-27 13:01:10 -0600

    Update Chapter 10 Notes

---
+++ .ipynb_checkpoints/salariedcommissionemployee-checkpoint.py
@@ -0,0 +1,34 @@
+# salariedcommissionemployee.py
+"""SalariedCommissionEmployee derived from CommissionEmployee."""
+from commissionemployee import CommissionEmployee
+from decimal import Decimal
+
+class SalariedCommissionEmployee(CommissionEmployee):
+    """An employee who gets paid a salary plus commission based on gross sales."""
+    
+    def __init__(self, first_name, last_name, ssn, gross_sales, commission_rate, base_salary):
+        """Initialize SalariedCommissionEmployee's attributes."""
+        super().__init__(first_name, last_name, ssn, gross_sales, commission_rate)
+        self.base_salary = base_salary # validate via property
+        
+    @property
+    def base_salary(self):
+        return self._base_salary
+        
+    @base_salary.setter
+    def base_salary(self, base_salary):
+        """Set base salary or raise ValueError if invalid."""
+        if base_salary < Decimal('0.00'):
+            raise ValueError('Base salary must be >= to 0')
+                
+        self._base_salary = base_salary
+            
+    def earnings(self):
+        """Calculate earnings."""
+        return super().earnings() + self.base_salary
+        
+    def __repr__(self):
+        """Return string representation for repr()."""
+        return('Salaried' + super().__repr__() + f'\nbase salary: {self.base_salary:.2f}')
+        
+        
